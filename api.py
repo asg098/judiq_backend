bc29547857a077232081a46008d989d9f2e3ec14
Atharva Gandhi <gandhiatharv565@gmail.com>
2026-04-27 20:56:13 +0530

    Update api.py

--- api.py
+++ api.py
@@ -249,3 +249,7 @@ async def generate_pdf(request: Request):
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
     uvicorn.run("api:app", host="0.0.0.0", port=port, reload=True)
+
+
+
+
